fd67fb6ef2c7016e49abf08f068d9bbe3c83b9e6
Hiran Junior <hiran.junior@rankmyapp.com.br>
2021-12-15 20:54:31 -0300

    first commit

---
+++ insta-scrapper/Controllers/Hello.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace insta_scrapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Hello : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHello()
+        {
+            var content = new { Hello = "World" };
+            return Ok(content);
+        }
+    }
+}
